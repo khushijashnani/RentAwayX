e9de89844d85d14f749af461099df9db673bd31f
RUCHA-NARGUNDE <48681640+RUCHA-NARGUNDE@users.noreply.github.com>
2019-10-11 20:49:41 +0530

    User java class and rent photo added

---
+++ app/src/main/java/com/project/rentaway/User.java
@@ -0,0 +1,54 @@
+package com.project.rentaway;
+
+public class User {
+
+   private String id,email,userName,password,phone;
+
+    public User(String id, String email, String userName, String password, String phone) {
+        this.id = id;
+        this.email = email;
+        this.userName = userName;
+        this.password = password;
+        this.phone = phone;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+}
